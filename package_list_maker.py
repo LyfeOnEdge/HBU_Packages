6550fce065f7128546618773c625f6b1205e696e
LyfeOnEdge <lyfeshomebrew@gmail.com>
2019-11-08 21:33:43 -0800

    add tool to make packages list, update packages.json

---
+++ package_list_maker.py
@@ -0,0 +1,10 @@
+import os, sys, json
+
+packages = []
+for package in os.listdir(sys.path[0]):
+	if os.path.isfile(os.path.join(sys.path[0], os.path.join(package, "package.json"))):
+		packages.append(package)
+
+print(json.dumps(packages, indent = 4))
+with open("packages.json", mode='w+') as packagefile:
+	json.dump(packages, packagefile, indent=4)
--- quick_converter.py
+++ quick_converter.py
@@ -1,23 +1,7 @@
 #Quickly converts a switchbru chunk to a package entry.
 import json, os, sys
 
-input = {
-            "category": "tool", 
-            "binary": "/switch/SimpleModManager/SimpleModManager.nro", 
-            "updated": "25/10/2019", 
-            "name": "SimpleModManager", 
-            "license": "GPLv3", 
-            "title": "SimpleModManager", 
-            "url": "https://github.com/nadrino/SimpleModManager/releases", 
-            "author": "nadrino", 
-            "changelog": "1.3.0\\n\\nAdding multiple presets in config file\\nPossibility to swap preset during the app execution with \"Y\"\\n\\n1.2.0\\n\\nAdding the possibility to change destination folder (Feature Request)\\nAdding parameter file reading (Initiated by kowbot, thanks !)\\nChanging keybinding\\nFixing bugs\\nCosmetics changes\\n\\n1.1.0\\n\\nCompiled with latest libnx (Compatible with 9.0.0)\\nFew changes in the ui\\nMinor bug fix\\n\\n1.0.0\\n\\nTested on Nintendo Switch Firmware : 8.1.0 and 9.0.0", 
-            "extracted": 985, 
-            "version": "1.3.0", 
-            "filesize": 372, 
-            "web_dls": 9, 
-            "details": "SimpleModManager is an homebrew app for the Nintendo Switch CFW : Atmosphere. It allows to manage your mods (via LayeredFS).\\n\\nAt the ROOT of your SDcard, there is a /mods/ folder.\\n\\nTree structure :\\n\\n/mods/<NameOfTheGame>/<NameOfTheMod>/<ModTreeStructureFromAtmosphereFolder>\\n\\nExample :\\n\\n /mods/The Legend of Zelda - Breath of the Wild/First Person View/titles/01007EF00011E000/romfs/Actor/Pack/GameRomCamera.sbactorpack", 
-            "description": "A Simple Game Mod Manager"
-        }
+input = None
 
 output = {
 			"name": "",
